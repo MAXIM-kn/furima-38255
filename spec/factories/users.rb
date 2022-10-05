8abb36d75d28f262fd8e7e222cbb03d19c1302c6
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-10-05 21:58:35 +0900

    ユーザー管理機能、userモデルテストの再修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname              { Faker::Name.name }
     email                 { Faker::Internet.free_email }
-    password              { Faker::Internet.password(min_length: 6) }
+    password              { 'test12' }
     password_confirmation { password }
     first_name            { person.first.kanji }
     last_name             { person.last.kanji }
