2ba4cf9f67e55dbb64a9263cfc7f8ae80e81c4ed
MAXIM-kn <bb0713t@yahoo.co.jp>
2022-11-28 14:26:56 +0900

    エラー修正

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Furima38255
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-    config.hosts << 'xxx.onrender.com'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
